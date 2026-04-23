61bfaaed872f9242cf952c685225846e8d5def4d
thatbulbul <bulbulsharma3363@gmail.com>
2026-04-23 14:59:39 +0530

    template literals

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("hello");
+//sum of a and b
+let a=10;
+let b=12;
+let output= "the sum of a and b " + (a+b) +" ruppes";
+console.log("the sub of a and b is",a-b);
+console.log(output);
+
+//template literals
+let temp=`using the template literals ${a+b} ruppess`;
+console.log(temp);
+
